2fd6839c6fc2c96cc81a0e4082623fcf0b3ec090
Brian <briandediego@gmail.com>
2018-05-12 08:56:05 -0300

    chequeo de acceso por array

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-Control de habitación integrado
+# Control de habitación integrado
+
+## Hardware
+
+* Modulo rfid: RFID-RC522 $60.45 x2
+* Arduino nano
+* Sensor de luz ldr
+* Sensor de movimiento: Modulo PIR HC-SR501 $56.68
+* Modulo wifi: esp8266 $108
+* Boozer piezoelectrico
+* Fotoresistencia LDR
+* Sensor magnetico
+* Pulsador
+* tags rfid
+* rele (para simular desbloqueo de puerta)
--- webService/lib/routes/home.js
+++ webService/lib/routes/home.js
@@ -1,10 +1,19 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
+const tarjetas = [{
+    id: '20d23ca8'
+}, {
+    id: '2eadba79'
+}]
 
-router.get('/access', function(req, res) {
-    let random = Math.random();
-    if(random > 0.5) {
+
+router.get('/access/:id', function(req, res) {
+    let tarjetasHabilitadas = tarjetas.filter((tarjetaElem) => {
+        return tarjetaElem.id === req.params.id;
+    });
+    
+    if(tarjetasHabilitadas.length) {
         res.status(200).send(true);
     } else {
         res.status(401).send(false);
