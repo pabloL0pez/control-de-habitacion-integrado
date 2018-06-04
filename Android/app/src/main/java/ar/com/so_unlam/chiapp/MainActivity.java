d90a5910833213f57bb11a0c1eb32e73ac40666b
pabloL0pez <pablo.lopez3895@gmail.com>
2018-06-03 21:10:18 -0300

    Empezando app de Android

---
+++ Android/app/src/main/java/ar/com/so_unlam/chiapp/MainActivity.java
@@ -0,0 +1,38 @@
+package ar.com.so_unlam.chiapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+    }
+
+}
